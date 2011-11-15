08066200a4168c8cd2c42298b022cc36a8f919a1
steflewandowski <stef@stef.io>
2011-11-15 10:21:33 +0000

    Story admin

---
+++ admin/controllers/stories.rb
@@ -0,0 +1,47 @@
+Admin.controllers :stories do
+
+  get :index do
+    @stories = Story.all
+    render 'stories/index'
+  end
+
+  get :new do
+    @story = Story.new
+    render 'stories/new'
+  end
+
+  post :create do
+    @story = Story.new(params[:story])
+    if @story.save
+      flash[:notice] = 'Story was successfully created.'
+      redirect url(:stories, :edit, :id => @story.id)
+    else
+      render 'stories/new'
+    end
+  end
+
+  get :edit, :with => :id do
+    @story = Story.get(params[:id])
+    render 'stories/edit'
+  end
+
+  put :update, :with => :id do
+    @story = Story.get(params[:id])
+    if @story.update(params[:story])
+      flash[:notice] = 'Story was successfully updated.'
+      redirect url(:stories, :edit, :id => @story.id)
+    else
+      render 'stories/edit'
+    end
+  end
+
+  delete :destroy, :with => :id do
+    story = Story.get(params[:id])
+    if story.destroy
+      flash[:notice] = 'Story was successfully destroyed.'
+    else
+      flash[:error] = 'Unable to destroy Story!'
+    end
+    redirect url(:stories, :index)
+  end
+end
